060c21a727db41e7304ba38a7e45792e7959142c
snehashis <snehashis.pal@students.iiit.ac.in>
2018-10-22 01:47:50 +0530

    Added a hello file to repo

---
+++ tests/threads/hello.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/init.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "devices/timer.h"
+
+void hello(void) {
+
+	printf("Hellow world");
+}
